31632d9a4f13068a71d4b5b9f865e387f2b7de16
Stheffany <stheffanyhadlich@gmail.com>
2019-10-05 01:44:08 -0300

    Create quiz component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Quiz />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from "./components/Quiz.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Quiz
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Quiz.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <h3>Who's That Pokémon?</h3>
+    </div>
+    <div>
+      <ul>
+        <li>Bulbasaur</li>
+        <li>Charmander</li>
+        <li>Squirtle</li>
+      </ul>
+    </div>
+  </div>
+</template>

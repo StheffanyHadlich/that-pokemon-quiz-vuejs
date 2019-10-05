17e79d7ddbbacfeb5bd21833bd241ee48d63664a
Stheffany <stheffanyhadlich@gmail.com>
2019-10-05 09:41:33 -0300

    Quiz layout

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    <div>
-      <h3>Who's That Pokémon?</h3>
-    </div>
-    <div>
-      <ul>
-        <li>Bulbasaur</li>
-        <li>Charmander</li>
-        <li>Squirtle</li>
-      </ul>
-    </div>
+  <div class="quiz-container">
+    <b-jumbotron >
+      <div>
+        <h3>Who's That Pokémon?</h3>
+      </div>
+      <hr class="my-4">
+      <div>
+         <b-form-group >
+          <b-form-radio v-model="selected" name="some-radios" value="Bulbasaur">Bulbasaur</b-form-radio>
+          <b-form-radio v-model="selected" name="some-radios" value="Charmander">Charmander</b-form-radio>
+          <b-form-radio v-model="selected" name="some-radios" value="Squirtle">Squirtle</b-form-radio>
+        </b-form-group>
+
+        <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+
+      </div>
+      <b-button variant="primary" href="#">More Info</b-button>
+    </b-jumbotron>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: ''
+    }
+  }
+}
+</script>

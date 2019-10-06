aa589c014538cbb243325aeb8662da3aa775529b
stheffany <stheffany@codeminer42.com>
2019-10-06 01:16:13 -0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div id="app">
     <b-container class="bv-example-row">
       <b-row>
-        <b-col sm="6" offset="3">
+        <b-col sm="4">
+          <Header v-if="questions.length" :index="index" :score="score" :total="total" />
+        </b-col>
+        <b-col sm="5">
           <Quiz
             v-if="questions.length"
             v-model="index"
             :options="questions[index].options"
             :rightAnswer="questions[index].rightAnswer"
             :score="score"
-            :selected="selected"
             :checkAnswer="checkAnswer"
             :onChange="onChange"
           />
@@ -22,11 +24,13 @@
 <script>
 import Quiz from "./components/Quiz.vue";
 import * as PokemonModel from "./models/PokemonModel";
+import Header from "./components/Header.vue";
 
 export default {
   name: "app",
   components: {
-    Quiz
+    Quiz,
+    Header
   },
   data: function() {
     return {
@@ -34,23 +38,32 @@ export default {
       index: 0,
       score: 0,
       selected: "",
+      total: 0,
     };
   },
   mounted: function() {
-    PokemonModel.getPokemons()
-    .then(questions => {
+    PokemonModel.getPokemons().then(questions => {
       this.questions = questions;
+      this.total = questions.length;
     });
   },
   methods: {
     checkAnswer: function() {
-      if(this.questions[this.index].rightAnswer.name === this.selected){
-        this.index++;
-        this.score++;
+      this.updateScore();
+      this.updateIndex();
+    },
+    updateIndex: function() {
+      if(this.index < this.total - 1)
+        return this.index++
+    },
+    updateScore: function () {
+      console.log(this.questions[this.index].rightAnswer.name, this.selected)
+      if(this.questions[this.index].rightAnswer.name == this.selected){
+        return this.score++;
       }
     },
     onChange: function(value) {
-      this.selected= value
+      this.selected = value;
     }
   }
 };

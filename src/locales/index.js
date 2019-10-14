8fa33b56672d57af1d4aa84ca5ce9b7468a9c4ef
Adriano Costa <adrianokosta001@gmail.com>
2019-10-14 20:35:01 -0300

    Add pt-BR and en locales to application

---
+++ src/locales/index.js
@@ -0,0 +1,7 @@
+import ptBr from './pt-BR'
+import en from './en'
+
+export default {
+  en,
+  'pt-BR': ptBr
+}
---
+++ src/locales/pt-BR.js
@@ -0,0 +1,15 @@
+export default {
+  'enter your name': 'Digite seu nome',
+  'start': 'Começar',
+  'player': 'Jogador',
+  'name': 'Nome',
+  'score': 'Pontuação',
+  'out of': 'de',
+  'questions': 'questões',
+  'Questions': 'Questões',
+  'play again': 'Jogar Novamente',
+  'game over': 'Fim de Jogo',
+  "Who's That Pokémon?": 'Quem é esse Pokemon?',
+  'Ok': 'Confirmar',
+  'Next': 'Próximo'
+}
